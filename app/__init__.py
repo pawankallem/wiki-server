e06ba37f30ee870be47a2a110fbdff5b9938b159
pawankallem <kallem.pavan1997@gmail.com>
2023-10-05 03:24:32 +0530

    for CORS added the deployed front end url

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/scrape/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/scrape/*": {"origins": ["http://localhost:5173","https://wikipedia-client-clone.vercel.app"]}})
 
 from app import routes
 
