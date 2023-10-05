c928cac5b91c6a86f52f0fdb60bb7a9bfab8ff70
pawankallem <kallem.pavan1997@gmail.com>
2023-10-05 09:52:29 +0530

    send suggetion data to frontend

--- app/routes.py
+++ app/routes.py
@@ -19,16 +19,24 @@ def suggetion_wikipedia(search_term):
     try:
         if not search_term:
             return jsonify({'error': 'Invalid search term'}), 400
-        wikipedia_api_url = f"{SUGGESTION_URL + search_term}"
-        response = requests.get(wikipedia_api_url)
+
+        url = f"{SUGGESTION_URL + search_term}"
+        response = requests.get(url)
+        response.raise_for_status()
         data = response.json()
-        search_results = data['query']['search']
         
-        scraped_data = [{'title': result['title'], 'snippet': result['snippet']} for result in search_results]
-        print("here : ",scraped_data)
-        return jsonify(scraped_data)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        suggestions = []
+        for i in range(len(data[1])):
+            suggestion = {
+                "name": data[1][i],
+                "image": data[3][i],
+                "description": get_description(data[1][i]),
+            }
+            suggestions.append(suggestion)
+        
+        return jsonify(suggestions)
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)})
 
 
 @app.route('/scrape/<search_term>')
@@ -49,3 +57,13 @@ def scrape_wikipedia(search_term):
         )
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+def get_description(title):
+    try:
+        response = requests.get(f"https://en.wikipedia.org/wiki/{title.replace(' ', '_')}")
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+        description = soup.find('p').text.strip()
+        return description
+    except requests.exceptions.RequestException:
+        return ""
