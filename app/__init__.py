ea9d62c0986c24a12d979530513221791be7c01a
pawankallem <kallem.pavan1997@gmail.com>
2023-10-05 07:25:55 +0530

    env added and showing suggetions in ui

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PY_ENV = os.getenv("PY_ENV")
+LOCAL_URL = os.getenv("LOCAL_URL")
+PROD_URL = os.getenv("PROD_URL")
+
+if PY_ENV == "local":
+    MY_URL = LOCAL_URL
+elif PY_ENV == "prod":
+    MY_URL = PROD_URL
 
 app = Flask(__name__)
-CORS(app, resources={r"/scrape/*": {"origins": ["http://localhost:5173","https://wikipedia-client-clone.vercel.app"]}})
+CORS(app, resources={
+    r"/scrape/*": {"origins": MY_URL},
+    r"/suggestion/*": {"origins": MY_URL}
+})
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,34 @@ from flask import request, jsonify
 from app import app
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SUGGESTION_URL = os.getenv("SUGGESTION_URL")
+WIKIPEDIA_URL = os.getenv("WIKIPEDIA_URL")
 
 @app.route('/')
 def index():
     return "Welcome to the Wikipedia scraper app!"
+
+@app.route('/suggestion/<search_term>')
+def suggetion_wikipedia(search_term):
+    try:
+        if not search_term:
+            return jsonify({'error': 'Invalid search term'}), 400
+        wikipedia_api_url = f"{SUGGESTION_URL + search_term}"
+        response = requests.get(wikipedia_api_url)
+        data = response.json()
+        search_results = data['query']['search']
+        
+        scraped_data = [{'title': result['title'], 'snippet': result['snippet']} for result in search_results]
+        print("here : ",scraped_data)
+        return jsonify(scraped_data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 @app.route('/scrape/<search_term>')
 def scrape_wikipedia(search_term):
@@ -13,12 +37,15 @@ def scrape_wikipedia(search_term):
         if not search_term:
             return jsonify({'error': 'Invalid search term'}), 400
         
-        wikipedia_api_url = f"https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch={search_term}"
+        wikipedia_api_url = f"{WIKIPEDIA_URL + search_term}"
         response = requests.get(wikipedia_api_url)
-        data = response.json()
-        search_results = data['query']['search']
+        temp = BeautifulSoup(response.text,"html.parser")
+
+        for paragraph in temp.select("p"):
+            print("hello",paragraph.getText())
         
-        scraped_data = [{'title': result['title'], 'snippet': result['snippet']} for result in search_results]
-        return jsonify(scraped_data)
+        return jsonify(
+            "hi"
+        )
     except Exception as e:
         return jsonify({'error': str(e)}), 500
